d2f036a2bfae697f95ae3aec7ce2b23947d31a56
Zeeshan Yousaf <zeeshanento34@gmail.com>
2023-10-31 15:24:08 +0500

    connected to database

--- api/index.js
+++ api/index.js
@@ -1,5 +1,17 @@
-import express from "express";
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const cors = require("cors");
+const connectDB = require("../config/db");
+dotenv.config();
+connectDB();
 const app = express();
+// middlewares
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
 app.listen(3000, () => {
   console.log("server is running on port 3000");
 });
---
+++ config/db.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const colors = require("colors");
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(
+      `connected to mongoDB database ${conn.connection.host}`.bgMagenta.black
+    );
+  } catch (error) {
+    console.log(`error in DB ${error}`.bgRed.white);
+  }
+};
+
+module.exports = connectDB;
