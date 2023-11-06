cf361fb282338f4c074884e8c677187c828e86e9
Zeeshan Yousaf <zeeshanento34@gmail.com>
2023-11-06 12:44:57 +0500

    complete sign up page functionality

--- api/controller/authController.js
+++ api/controller/authController.js
@@ -5,6 +5,17 @@ const signUp = async (req, res, next) => {
   const { username, email, password } = req.body;
   console.log(req.body);
   try {
+    const existingUser = await User.findOne({ $or: [{ email }, { username }] });
+    if (existingUser) {
+      if (existingUser.username === username) {
+        // Username is already in use
+        return next(errorHandler(409, "Duplicate username"));
+      }
+      if (existingUser.email === email) {
+        // Email is already in use
+        return next(errorHandler(409, "Duplicate email "));
+      }
+    }
     const hashedPassword = bcryptjs.hashSync(password, 10);
     const newUser = new User({ username, email, password: hashedPassword });
     await newUser.save();
