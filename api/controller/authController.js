30905fe1fc682904d19ea2b7bb8f23d9301b889a
Zeeshan Yousaf <zeeshanento34@gmail.com>
2023-11-07 17:07:15 +0500

    create sign in api route

--- api/controller/authController.js
+++ api/controller/authController.js
@@ -1,6 +1,7 @@
 const User = require("../model/user");
 const bcryptjs = require("bcryptjs");
 const errorHandler = require("../utils/error");
+const jwt = require("jsonwebtoken");
 const signUp = async (req, res, next) => {
   const { username, email, password } = req.body;
   console.log(req.body);
@@ -25,5 +26,36 @@ const signUp = async (req, res, next) => {
     // next(errorHandler(501, "error from my sde"));
   }
 };
+const signIn = async (req, res, next) => {
+  const { email, password } = req.body;
+  console.log(req.body);
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      // Email is already in use
+      return next(errorHandler(404, "user not found "));
+    }
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      // Email is already in use
+      return next(errorHandler(401, "invalid credentials "));
+    }
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...rest } = validUser._doc;
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+        expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
+      })
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    next(error);
+    // next(errorHandler(501, "error from my sde"));
+  }
+};
 
-module.exports = signUp;
+module.exports = {
+  signUp,
+  signIn,
+};
--- api/routes/authRoute.js
+++ api/routes/authRoute.js
@@ -1,6 +1,7 @@
 const express = require("express");
-const signUp = require("../controller/authController");
+const { signUp, signIn } = require("../controller/authController");
 
 const router = express.Router();
 router.post("/signup", signUp);
+router.post("/sigin", signIn);
 module.exports = router;
