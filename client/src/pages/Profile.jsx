1bc14fd736d65e7a79644ce6377e92f8dc23b075
Zeeshan Yousaf <zeeshanento34@gmail.com>
2023-11-11 08:58:42 +0500

    complete user profile

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,7 +1,8 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { useSelector } from "react-redux";
 
 const Profile = () => {
+  const fileRef = useRef(null);
   const { currentUser } = useSelector((state) => state.user);
   const [formData, setFormData] = useState({});
 
@@ -16,10 +17,12 @@ const Profile = () => {
       <div className="p-3 max-w-lg mx-auto">
         <h1 className="text-3xl text-center my-7">Profile</h1>
         <form className="flex flex-col gap-4">
+          <input type="file" ref={fileRef} hidden accept="image/*" />
           <img
             src={currentUser.avatar}
             alt="profile image"
             className="rounded-full w-24 h-24 object-cover cursor-pointer self-center mt-2"
+            onClick={() => fileRef.current.click()}
           />
           <input
             type="text"
