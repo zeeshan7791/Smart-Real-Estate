ef96bc88e3c0879533873c3926cbcef39156f8d0
Zeeshan Yousaf <zeeshanento34@gmail.com>
2024-01-08 22:38:03 +0500

    upload file middleware added to route

--- api/routes/authRoute.js
+++ api/routes/authRoute.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const { signUp, signIn, signOut } = require("../controller/authController");
-
+const upload = require("../middleware/upload");
 const router = express.Router();
-router.post("/signup", signUp);
+router.post("/signup", upload.single("photo"), signUp);
 router.post("/signin", signIn);
 router.get("/signout", signOut);
 module.exports = router;
