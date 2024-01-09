ca35a3c6bd23d515c768a747d61c687ba8389270
Zeeshan Yousaf <zeeshanento34@gmail.com>
2024-01-09 21:57:21 +0500

    user route update with upload file

--- api/routes/useroute.js
+++ api/routes/useroute.js
@@ -4,10 +4,11 @@ const {
   updateUser,
   deleteUser,
 } = require("../controller/userController");
+const upload = require("../middleware/upload");
 const { verifyToken } = require("../utils/verifyUser");
 const router = express.Router();
 
 router.get("/test", getUser);
-router.post("/update/:id", verifyToken, updateUser);
+router.post("/update/:id", verifyToken, upload.single("photo"), updateUser);
 router.delete("/delete/:id", verifyToken, deleteUser);
 module.exports = router;
