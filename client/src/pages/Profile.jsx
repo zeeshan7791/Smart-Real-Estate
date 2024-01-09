798d8f1076602e36f4ececfd39d8d5c25423c635
Zeeshan Yousaf <zeeshanento34@gmail.com>
2024-01-09 21:58:37 +0500

    update user information with user profile photo

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import {
   updateUserStart,
@@ -12,27 +12,54 @@ import {
   signoutUserSuccess,
 } from "../redux/user/userSlice";
 const Profile = () => {
-  const fileRef = useRef(null);
   const { currentUser, loading, error } = useSelector((state) => state.user);
-  const [formData, setFormData] = useState({});
+  const [updateValue, setUpdateData] = useState({});
   const [update, setUpdate] = useState(false);
+  const [photo, setPhoto] = useState(null);
   const dispatch = useDispatch();
   const handleChange = (e) => {
-    setFormData({
-      ...formData,
+    setUpdateData({
+      ...updateValue,
       [e.target.id]: e.target.value,
     });
   };
+  // logOutUser
+  const handleSignOut = async () => {
+    try {
+      dispatch(signoutUserStart());
+      const res = await fetch("api/auth/signout");
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(signoutUserFailure(data.message));
+        return;
+      }
+      dispatch(signoutUserSuccess());
+    } catch (error) {
+      dispatch(signoutUserFailure(error.message));
+    }
+  };
+  // updateUserInformation
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
+      const updateForm = new FormData();
+      updateForm.append(
+        "username",
+        updateValue.username ? updateValue.username : currentUser.username
+      );
+      updateForm.append(
+        "email",
+        updateValue.email ? updateValue.email : currentUser.email
+      );
+      updateForm.append(
+        "password",
+        updateValue.password ? updateValue.password : ""
+      );
+      updateForm.append("photo", photo);
       dispatch(updateUserStart());
       const res = await fetch(`api/user/update/${currentUser._id}`, {
         method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(formData),
+        body: updateForm,
       });
       const data = await res.json();
       // console.log(data);
@@ -42,6 +69,7 @@ const Profile = () => {
       }
       dispatch(updateUserSuccess(data));
       setUpdate(true);
+      handleSignOut();
     } catch (error) {
       dispatch(updateUserFailure(error.message));
     }
@@ -65,56 +93,63 @@ const Profile = () => {
     }
   };
 
-  const handleSignOut = async () => {
-    try {
-      dispatch(signoutUserStart());
-      const res = await fetch("api/auth/signout");
-      const data = await res.json();
-      if (data.success === false) {
-        dispatch(signoutUserFailure(data.message));
-        return;
-      }
-      dispatch(signoutUserSuccess());
-    } catch (error) {
-      dispatch(signoutUserFailure(error.message));
-    }
-  };
   return (
     <>
       <div className="p-3 max-w-lg mx-auto">
         <h1 className="text-3xl text-center my-7">Profile</h1>
         <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
-          <input type="file" ref={fileRef} hidden accept="image/*" />
-          <img
-            src={currentUser.avatar}
-            alt="profile image"
-            className="rounded-full w-24 h-24 object-cover cursor-pointer self-center mt-2"
-            onClick={() => fileRef.current.click()}
-          />
+          <div className="mb-3">
+            <label className="btn btn-outline-secondary col-md-12">
+              <div className=" m-auto w-fit">
+                {photo ? (
+                  <div className="text-center">
+                    <img
+                      src={URL.createObjectURL(photo)}
+                      alt="product-photo"
+                      className="w-16 h-16 rounded-full"
+                    />
+                  </div>
+                ) : (
+                  <div className="text-center">
+                    <img
+                      src={`http://localhost:3000/image/${currentUser.photo}`}
+                      alt="product_photo"
+                      className="w-16 h-16 rounded-full"
+                    />
+                  </div>
+                )}
+              </div>
+              <p className="text-center">
+                {photo ? photo.name : "Upload Photo"}
+              </p>
+              <input
+                type="file"
+                name="photo"
+                className="form-control"
+                placeholder="Enter Product photo"
+                accept="image/*"
+                onChange={(e) => setPhoto(e.target.files[0])}
+                hidden
+              />
+            </label>
+          </div>
           <input
             type="text"
-            placeholder="username"
             defaultValue={currentUser.username}
+            placeholder="username"
             className="border p-3 rounded-lg  "
             id="username"
             onChange={handleChange}
           />
           <input
             type="email"
-            placeholder="email"
             defaultValue={currentUser.email}
+            placeholder="email"
             className="border p-3 rounded-lg  "
             id="email"
             onChange={handleChange}
           />
-          <input
-            type="text"
-            placeholder="profile URL"
-            defaultValue={currentUser.avatar}
-            className="border p-4 rounded-lg  "
-            id="avatar"
-            onChange={handleChange}
-          />
+
           <input
             type="password"
             placeholder="password"
