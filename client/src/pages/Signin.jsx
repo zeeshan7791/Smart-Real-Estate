53740415e79440bcdd1585f344cbea764095d299
Zeeshan Yousaf <zeeshanento34@gmail.com>
2024-01-09 21:59:48 +0500

    correct spelling

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -56,7 +56,7 @@ const Signin = () => {
   return (
     <>
       <div className="p-3 max-w-lg mx-auto">
-        <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
+        <h1 className="text-3xl text-center font-semibold my-7">Sign In</h1>
         <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
           <input
             type="email"
