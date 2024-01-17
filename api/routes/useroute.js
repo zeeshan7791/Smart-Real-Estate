914f1a2abef09fc08ad9673e140bf43ed84119a0
Zeeshan Yousaf <zeeshanento34@gmail.com>
2024-01-17 22:38:28 +0500

    intial commit

--- api/routes/useroute.js
+++ api/routes/useroute.js
@@ -3,6 +3,7 @@ const {
   getUser,
   updateUser,
   deleteUser,
+  getUserListings,
 } = require("../controller/userController");
 const upload = require("../middleware/upload");
 const { verifyToken } = require("../utils/verifyUser");
@@ -11,4 +12,5 @@ const router = express.Router();
 router.get("/test", getUser);
 router.post("/update/:id", verifyToken, upload.single("photo"), updateUser);
 router.delete("/delete/:id", verifyToken, deleteUser);
+router.get("/listings/:id", verifyToken, getUserListings);
 module.exports = router;
