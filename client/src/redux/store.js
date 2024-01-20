1a5cd66cbca6f77e6546fa427f1366d8f7970634
Zeeshan Yousaf <zeeshanento34@gmail.com>
2024-01-20 22:30:52 +0500

    listing store created to manage the state

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -3,14 +3,19 @@ import userReducer from "./user/userSlice";
 import { persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import persistStore from "redux-persist/es/persistStore";
+import listingSlice from "./listing/listingSlice";
 
-const rootReducer = combineReducers({ user: userReducer });
+const rootReducer = combineReducers({
+  user: userReducer,
+  listings: listingSlice,
+});
 const persistConfig = {
   key: "root",
   storage,
   version: 1,
 };
 const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
   reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
