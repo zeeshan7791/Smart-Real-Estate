fc243bc2fddd800212d54234838515f2d068d7e6
Zeeshan Yousaf <zeeshanento34@gmail.com>
2024-01-21 13:07:00 +0500

    user own listing api created

--- api/controller/listingController.js
+++ api/controller/listingController.js
@@ -32,7 +32,7 @@ const createListing = async (req, res, next) => {
 const getListing = async (req, res, next) => {
   try {
     const getProductListing = await Listing.find();
-    console.log(getProductListing);
+    // console.log(getProductListing);
     res.status(200).json({
       success: true,
       message: "listing get successfully",
@@ -42,7 +42,47 @@ const getListing = async (req, res, next) => {
     next(error);
   }
 };
+
+const getMyListings = async (req, res, next) => {
+  try {
+    // console.log(req.body, "value in body");
+    const { userRef } = req.body;
+    console.log(userRef);
+    const userListing = await Listing.find({ userRef });
+    console.log(userListing, "value in listing--------------");
+    res.status(200).json({
+      success: true,
+      message: "user listing fetched successfully",
+      userListing,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+const updateListing = async (req, res, next) => {
+  try {
+    const {
+      name,
+      description,
+      address,
+      regularPrice,
+      discountPrice,
+      bedrooms,
+      bathrooms,
+      furnished,
+      parking,
+      type,
+      offer,
+      userRef,
+    } = req.body;
+
+    const updatelistings = await Listing.findByIdAndUpdate();
+  } catch (error) {
+    next(error);
+  }
+};
 module.exports = {
   createListing,
   getListing,
+  getMyListings,
 };
--- api/routes/listingRoute.js
+++ api/routes/listingRoute.js
@@ -2,11 +2,13 @@ const express = require("express");
 const {
   createListing,
   getListing,
+  getMyListings,
 } = require("../controller/listingController");
 const router = express.Router();
 const upload = require("../middleware/upload");
 
 router.post("/create-listing", upload.array("pictures"), createListing);
 router.get("/get-listing", getListing);
+router.post("/my-listing", getMyListings);
 
 module.exports = router;
