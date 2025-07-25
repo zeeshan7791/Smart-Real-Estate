bca1e15c935ccb24320e4ecfdc28150614463211
Zeeshan Yousaf <78100777+zeeshan7791@users.noreply.github.com>
2025-07-25 18:08:08 +0500

    change to import es-6 module

--- api/routes/authRoute.js
+++ api/routes/authRoute.js
@@ -1,8 +1,11 @@
-const express = require("express");
-const { signUp, signIn, signOut } = require("../controller/authController");
-const upload = require("../middleware/upload");
+import express from "express";
+import { signUp, signIn, signOut } from "../controller/authController.js";
+import upload from "../middleware/upload.js";
+
 const router = express.Router();
+
 router.post("/signup", upload.single("photo"), signUp);
 router.post("/signin", signIn);
 router.get("/signout", signOut);
-module.exports = router;
+
+export default router;
