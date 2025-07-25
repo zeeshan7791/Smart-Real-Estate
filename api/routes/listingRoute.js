105de357b7459fc75196ab1272848e0cf4c9d02c
Zeeshan Yousaf <78100777+zeeshan7791@users.noreply.github.com>
2025-07-25 18:08:51 +0500

    change to import es-6 module

--- api/routes/listingRoute.js
+++ api/routes/listingRoute.js
@@ -1,31 +1,22 @@
-const express = require("express");
-const {
+import express from "express";
+import {
   createListing,
   getMyListings,
   deleteListing,
   updateListing,
   getSingleListing,
   getListings,
-} = require("../controller/listingController");
+} from "../controller/listingController.js";
+import upload from "../middleware/upload.js";
+import { verifyToken } from "../utils/verifyUser.js";
+
 const router = express.Router();
-const upload = require("../middleware/upload");
-const { verifyToken } = require("../utils/verifyUser");
 
-router.post(
-  "/create-listing",
-
-  upload.array("pictures"),
-  createListing
-);
+router.post("/create-listing", upload.array("pictures"), createListing);
 router.post("/my-listing", getMyListings);
 router.delete("/delete/:id", verifyToken, deleteListing);
-router.post(
-  "/update/:id",
-  upload.array("pictures"),
-  verifyToken,
-  updateListing
-);
+router.post("/update/:id", upload.array("pictures"), verifyToken, updateListing);
 router.get("/getListing/:id", getSingleListing);
 router.get("/get", getListings);
 
-module.exports = router;
+export default router;
