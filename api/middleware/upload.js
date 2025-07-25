055357734975126639ea8bd13d4481d36aabfa8e
Zeeshan Yousaf <78100777+zeeshan7791@users.noreply.github.com>
2025-07-25 20:44:03 +0500

    fix path of upload

--- api/middleware/upload.js
+++ api/middleware/upload.js
@@ -1,9 +1,14 @@
 import multer from "multer";
+import path from "path";
+import { fileURLToPath } from "url";
 
+// Manually define __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "api/uploads/"); // Make sure 'api/uploads/' matches the actual path
+    cb(null, path.join(__dirname, "../uploads")); // Resolves correctly for ES modules
   },
   filename: (req, file, cb) => {
     cb(null, file.originalname);
