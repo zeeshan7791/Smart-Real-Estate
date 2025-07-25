a883bcc7a8fbbf186c466977c49a5f3426a21f3b
Zeeshan Yousaf <78100777+zeeshan7791@users.noreply.github.com>
2025-07-25 18:07:17 +0500

    change to import es-6 module

--- api/model/user.js
+++ api/model/user.js
@@ -1,4 +1,4 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
   {
@@ -11,4 +11,7 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
-module.exports = mongoose.model("User", userSchema);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
