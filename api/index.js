a2d1a290957b5643fae66dacae8e311bdaa6ea4c
Zeeshan Yousaf <78100777+zeeshan7791@users.noreply.github.com>
2025-07-25 19:03:12 +0500

    fix image path

--- api/index.js
+++ api/index.js
@@ -24,13 +24,14 @@ app.use(cookieParser());
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
+app.use("/image", express.static(path.join(__dirname, "uploads")));
 const clientDistPath = path.join(__dirname, "..", "client", "dist");
 app.use(express.static(clientDistPath));
 app.get("*", (req, res) => {
   res.sendFile(path.join(clientDistPath, "index.html"));
 });
 const PORT = process.env.PORT || 5000;
-app.use("/image", express.static(path.join(__dirname, "uploads")));
+
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
 app.use((err, req, res, next) => {
