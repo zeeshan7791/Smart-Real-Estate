b0bb3870866f2f08c633dce7a43e3fc8289b02b7
Zeeshan Yousaf <78100777+zeeshan7791@users.noreply.github.com>
2025-07-25 18:09:25 +0500

    change to import es-6 module

--- api/routes/useroute.js
+++ api/routes/useroute.js
@@ -1,15 +1,16 @@
-const express = require("express");
-const {
+import express from "express";
+import {
   getUser,
   updateUser,
   deleteUser,
-} = require("../controller/userController");
-const upload = require("../middleware/upload");
-const { verifyToken } = require("../utils/verifyUser");
+} from "../controller/userController.js";
+import upload from "../middleware/upload.js";
+import { verifyToken } from "../utils/verifyUser.js";
+
 const router = express.Router();
 
 router.post("/update/:id", verifyToken, upload.single("photo"), updateUser);
 router.delete("/delete/:id", verifyToken, deleteUser);
 router.get("/:id", verifyToken, getUser);
 
-module.exports = router;
+export default router;
